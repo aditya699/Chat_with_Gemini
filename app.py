70ba2ce7c060bbb453f4c526d66f8086bbe3f9ec
Aditya Bhatt <meenakshi.bhtt@gmail.com>
2024-05-24 08:26:28 +0530

    Basic audio qna implemented

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+'''
+Features -
+1.Basic ask and response via gemini.This will help students get response in a efficent manner.
+'''
+
+
+
 import os
 import getpass
 from dotenv import load_dotenv
@@ -23,10 +30,9 @@ os.environ['GOOGLE_API_KEY']=google_api_key
 llm = ChatGoogleGenerativeAI(model="gemini-pro")
 
 start_of_conversation=utils.get_text()
-prompt_one = ChatPromptTemplate.from_template("Explain the following {concept} to a blind student in a concise and clear manner, ensuring that the key points are easily understood when the text is converted to audio.")
+prompt_one = ChatPromptTemplate.from_template("Explain the following {concept} to a blind student in a concise and clear manner, ensuring that the output is simple summary paragraph which doesn't contain any special chracters")
 output_parser = StrOutputParser()
 chain = prompt_one | llm | output_parser
 result=chain.invoke(f"{start_of_conversation}")
-print(result)
 engine.say(result)
 engine.runAndWait()
