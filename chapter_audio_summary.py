ae4c1384d44432a35b83b4ed2961e812e4e9e73b
Aditya Bhatt <meenakshi.bhtt@gmail.com>
2024-05-27 17:02:10 +0530

    translation feature added using gpt4-o

---
+++ chapter_audio_summary.py
@@ -0,0 +1,6 @@
+'''
+Objective -
+1.Create audio summary for all chapters
+
+For Demo Purpose i will be showing for civics book for class 7
+'''
---
+++ translation.py
@@ -0,0 +1,26 @@
+'''
+In case any teacher wants to use translation they can use this right know this will convert to hindi
+'''
+
+
+
+import os
+import openai
+from langchain_openai import AzureOpenAI
+from langchain_core.messages import HumanMessage
+from langchain_openai import AzureChatOpenAI
+os.environ["AZURE_OPENAI_API_VERSION"] = "2023-06-01-preview"
+os.environ["AZURE_OPENAI_ENDPOINT"]="https://aiagents.openai.azure.com/"
+os.environ["AZURE_OPENAI_API_KEY"]="198e8394cb0f47e69807d2bcd7a26c60"
+os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"] = "test_4o"
+model = AzureChatOpenAI(
+    openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+    azure_deployment=os.environ["AZURE_OPENAI_CHAT_DEPLOYMENT_NAME"],
+)
+text=input("Enter the language you want to covert text into : ")
+text_input=input("Enter the text : ")
+
+message = HumanMessage(
+    content=f"Translate {text_input} sentence from English to {text}"
+)
+print(model.invoke([message]).content)
